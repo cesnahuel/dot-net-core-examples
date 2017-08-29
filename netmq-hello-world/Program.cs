fce4d6b2f45b983f268fd1c8d8c459f3fe1e29e3
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-08-29 15:54:03 +0200

    add simple netmq example

---
+++ netmq-hello-world/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Threading;
+
+
+namespace NetMQHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            while (true)
+            {
+                int port = rand.Next(49152, 65535);
+                string address = $"tcp://localhost:{port}";
+                try
+                {
+                    Console.WriteLine($"Trying to use '{address}'");
+                    using (var server = new ResponseSocket($"@{address}"))
+                    using (var client = new RequestSocket($">{address}"))
+                    {
+                        while (true)
+                        {
+                            client.SendFrame("Hello");
+                            Console.WriteLine($"From client: {server.ReceiveFrameString()}");
+
+                            server.SendFrame("Hi Back");
+                            Console.WriteLine($"From server: {client.ReceiveFrameString()}");
+
+                            Thread.Sleep(1000);
+                        }
+                    }
+                }
+                catch(AddressAlreadyInUseException)
+                {
+                    Console.WriteLine($"This address is in use. Let's pick onther one.");
+                }
+            }
+        }
+    }
+}
---
+++ netmq-hello-world/readme.md
@@ -0,0 +1,5 @@
+# NetMQ example #
+
+add NetMQ package to the project
+
+    dotnet add package NetMQ --version 4.0.0.1
