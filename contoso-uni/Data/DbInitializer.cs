08feb3f93a0964401f8feca5d62aad5b81a43eb3
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-08-29 09:10:41 +0200

    fix identation

--- contoso-uni/Data/DbInitializer.cs
+++ contoso-uni/Data/DbInitializer.cs
@@ -36,13 +36,13 @@ namespace ContosoUniversity.Data
 
             var courses = new Course[]
             {
-                new Course { CourseId=1050, Title="Chemistry",Credits=3},
-                new Course { CourseId=4022, Title="Microeconomics",Credits=3},
-                new Course { CourseId=4041, Title="Macroeconomics",Credits=3},
-                new Course { CourseId=1045, Title="Calculus",Credits=4},
-                new Course { CourseId=3141, Title="Trigonometry",Credits=4},
-                new Course { CourseId=2021, Title="Composition",Credits=3},
-                new Course { CourseId=2042, Title="Literature",Credits=4}
+                new Course { CourseId=1050, Title="Chemistry", Credits=3 },
+                new Course { CourseId=4022, Title="Microeconomics", Credits=3 },
+                new Course { CourseId=4041, Title="Macroeconomics", Credits=3 },
+                new Course { CourseId=1045, Title="Calculus", Credits=4},
+                new Course { CourseId=3141, Title="Trigonometry", Credits=4 },
+                new Course { CourseId=2021, Title="Composition", Credits=3 },
+                new Course { CourseId=2042, Title="Literature", Credits=4 }
             };
             foreach (Course c in courses)
             {
