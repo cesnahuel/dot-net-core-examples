87ed2ecfba2386c995a97541e138fd3533e9d279
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-08-31 09:18:53 +0200

    add multi project structure based on solution

---
+++ netmq-hello-world-separate-processes/message/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NetMQHelloWorld
+{
+    public class Class1
+    {
+    }
+}
---
+++ netmq-hello-world-separate-processes/server/Server.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NetMQHelloWorld
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
