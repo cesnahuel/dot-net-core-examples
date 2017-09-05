b984d915b4205943de73c46e07c5b26c5b7b977f
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-09-05 16:57:08 +0200

    show qoutes line of a txt file

--- qotd/Program.cs
+++ qotd/Program.cs
@@ -1,13 +1,27 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 
 namespace QotD
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Console.Error.WriteLineAsync("You must specify a quotes file.");
+                Console.ResetColor();
+                return 1;
+            }
+
+            string[] quotes = await File.ReadAllLinesAsync(args[0]);
+            string randomQuote = quotes[new Random().Next(0, quotes.Length - 1)];
+            await Console.Out.WriteLineAsync($"[QOTD]: {randomQuote}");
+
+            return 0;
         }
     }
 }
