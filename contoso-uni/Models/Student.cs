3cedd85679c3519e4ddfe39adaeca6ea52e63a74
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-09-17 11:29:31 +0200

    add display annotation to the model fields

--- contoso-uni/Models/Student.cs
+++ contoso-uni/Models/Student.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace ContosoUniversity.Models
@@ -7,8 +8,12 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int StudentId { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [Display(Name = "Erollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
