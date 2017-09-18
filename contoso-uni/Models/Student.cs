ad94ef3121945a422d37fea88021a0fc299e8383
Evgeny Bobkin <evgen.ibqn@gmail.com>
2017-09-18 20:11:56 +0200

    add property length

--- contoso-uni/Models/Student.cs
+++ contoso-uni/Models/Student.cs
@@ -9,12 +9,16 @@ namespace ContosoUniversity.Models
     {
         public int StudentId { get; set; }
         [Display(Name = "Last Name")]
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters.")]
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
         [Display(Name = "Enrollment Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
