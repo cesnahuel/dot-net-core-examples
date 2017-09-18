ccd9c63f5a25380e1ce9e532e94d22a75e61cd16
Evgeny Bobkin <evgen.ibqn@gmail.com>
2017-09-18 20:16:57 +0200

    add migration for max length of names

--- contoso-uni/Migrations/20170918181417_MaxLengthOnNames.Designer.cs
+++ contoso-uni/Migrations/20170918181417_MaxLengthOnNames.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace contosouni.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170918181417_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
