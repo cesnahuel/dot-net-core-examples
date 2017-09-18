268ad19d09d15983376619748b933b4b3a45172b
Evgeny Bobkin <evgen.ibqn@gmail.com>
2017-09-18 20:24:36 +0200

    add migration for column change

--- contoso-uni/Migrations/20170918182228_ColumnFirstName.Designer.cs
+++ contoso-uni/Migrations/20170918182228_ColumnFirstName.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace contosouni.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170918182228_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ contoso-uni/Migrations/20170918182228_ColumnFirstName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace contosouni.Migrations
+{
+    public partial class ColumnFirstName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "students",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "students",
+                newName: "FirstMidName");
+        }
+    }
+}
