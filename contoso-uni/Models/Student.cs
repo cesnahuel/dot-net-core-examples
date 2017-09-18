905d08093d1227252b7a10da7eae4968336287a4
Evgeny Bobkin <evgen.ibqn@gmail.com>
2017-09-18 23:07:06 +0200

    add required attribute and comuted property

--- contoso-uni/Models/Student.cs
+++ contoso-uni/Models/Student.cs
@@ -9,17 +9,27 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int StudentId { get; set; }
+        [Required]
         [Display(Name = "Last Name")]
         [StringLength(50)]
         public string LastName { get; set; }
         [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters.")]
         [Display(Name = "First Name")]
         [Column("FirstName")]
+        [Required]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [Display(Name = "Enrollment Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstMidName}";
+            }
+
+        }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
